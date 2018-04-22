faac7dfc09713aef60c7aeb88a0d9d6318081c0f
Antti Haavikko <antti.haavikko@focus.fi>
2018-04-22 18:29:46 +0300

    Update build pushing script

--- pushbuilds.sh
+++ pushbuilds.sh
@@ -4,13 +4,13 @@ NC='\033[0m' # No Color
 echo " "
 
 echo "${COLOR}Pushing build for OSX${NC}"
-butler push Builds/osx anttihaavikko/tic-tac-matrix:osx --fix-permissions
+butler push Builds/osx anttihaavikko/binary-blocks:osx --fix-permissions
 
 echo "${COLOR}Pushing build for Windows${NC}"
-butler push Builds/win anttihaavikko/tic-tac-matrix:win --fix-permissions
+butler push Builds/win anttihaavikko/binary-blocks:win --fix-permissions
 
 echo "${COLOR}Pushing build for Linux${NC}"
-butler push Builds/linux anttihaavikko/tic-tac-matrix:linux
+butler push Builds/linux anttihaavikko/binary-blocks:linux
 
 echo "${COLOR}Pushing build for HTML5${NC}"
-butler push Builds/html5 anttihaavikko/tic-tac-matrix:html5
+butler push Builds/html5 anttihaavikko/binary-blocks:html5
